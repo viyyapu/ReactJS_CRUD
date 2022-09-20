40bf8f986c571dea31956a520516588ff97c3445
Viyyapu Sirisha <viyyapu.sirisha@ltts.com>
2022-09-20 16:48:29 +0530

    second

---
+++ src/components/Employees.js
@@ -0,0 +1,17 @@
+const Employees = [
+{
+    id:"1",
+    Name:"Viyyapu Sirisha",
+    Age:"22"
+},{
+    id:"2",
+    Name:"Juttika Omsai",
+    Age:"23"
+},{
+    id:"3",
+    Name:"Penki Saisree",
+    Age:"22"
+}
+]
+
+export default Employees;
